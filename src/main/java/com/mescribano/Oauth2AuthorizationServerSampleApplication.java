8533e8affba681bd208ff97d8e19c7a3b155e6ea
Miguel Ángel Escribano <maescribano85@gmail.com>
2016-11-19 17:30:44 +0100

    Initial commit

---
+++ src/main/java/com/mescribano/Oauth2AuthorizationServerSampleApplication.java
@@ -0,0 +1,12 @@
+package com.mescribano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2AuthorizationServerSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2AuthorizationServerSampleApplication.class, args);
+	}
+}
