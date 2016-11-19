c6dafe31bff331d6f1ed5fb5ea3ce47e66330b49
Miguel Ángel Escribano <maescribano85@gmail.com>
2016-11-19 18:01:30 +0100

    feat(properties): adds security properties to test server endpoints

--- src/main/java/com/mescribano/oauth2server/Oauth2AuthorizationServerSampleApplication.java
+++ src/main/java/com/mescribano/oauth2server/Oauth2AuthorizationServerSampleApplication.java
@@ -1,9 +1,11 @@
-package com.mescribano;
+package com.mescribano.oauth2server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @SpringBootApplication
+@EnableAuthorizationServer
 public class Oauth2AuthorizationServerSampleApplication {
 
 	public static void main(String[] args) {
